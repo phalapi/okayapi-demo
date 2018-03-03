e77ec40ccb308498a0ac0058bc0ead7c5f248341
dogstar <chanzonghuang@gmail.com>
2018-03-03 23:54:34 +0800

    up

--- soeasy/comments.js
+++ soeasy/comments.js
@@ -1,3 +1,63 @@
+  Vue.component("page",{
+      template:"#page",
+        data:function(){
+            let allpage = 0
+
+            let _self = this
+            let params = { 
+                s: 'App.Main_Set.Count', 
+                key: 'demo_comments'
+            }
+
+            $.ajax({
+                url: '/okayapi.php',
+                dataType: 'json',
+                data: params
+            }).done(function (rs) {
+                if (rs.data && rs.data.err_code == 0) {
+                    allpage = rs.data.total
+                } else {
+                    // console.log(rs.data.err_msg)
+                }
+            });
+
+          return{
+            current: 1,
+            showItem: 5,
+            allpage: allpage
+          }
+        },
+        computed:{
+          pages:function(){
+                var pag = [];
+                  if( this.current < this.showItem ){ //如果当前的激活的项 小于要显示的条数
+                       //总页数和要显示的条数那个大就显示多少条
+                       var i = Math.min(this.showItem,this.allpage);
+                       while(i){
+                           pag.unshift(i--);
+                       }
+                   }else{ //当前页数大于显示页数了
+                       var middle = this.current - Math.floor(this.showItem / 2 ),//从哪里开始
+                           i = this.showItem;
+                       if( middle >  (this.allpage - this.showItem)  ){
+                           middle = (this.allpage - this.showItem) + 1
+                       }
+                       while(i--){
+                           pag.push( middle++ );
+                       }
+                   }
+                 return pag
+               }
+      },
+      methods:{
+        goto:function(index){
+          if(index == this.current) return;
+            this.current = index;
+            //这里可以发送ajax请求
+        }
+      }
+    })
+
 var appComments = new Vue({
     el: '#appComments',
     data: {
