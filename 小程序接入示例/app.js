00b4223173d2494199b90a9cc9d3b9f6e45b6d3b
dogstar <chanzonghuang@gmail.com>
2018-03-25 00:05:52 +0800

    up

---
+++ 小程序接入示例/app.js
@@ -0,0 +1,42 @@
+//app.js
+App({
+  onLaunch: function () {
+    // 展示本地存储能力
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
+  },
+  globalData: {
+    userInfo: null,
+    okayapiHost: "https://api.okayapi.com", // TODO: 配置成你所在的接口域名
+    okayApiAppKey: "16BD4337FB1D355902E0502AFCBFD4DF", // TODO：改为你的APP_KEY
+    okayApiAppSecrect: "4c1402596e4cd017eeaO670df6f8B6783475b4ac8A32B4900f20abP2159711ad" // TODO：改为你的APP_SECRECT
+  }
+})
---
+++ 小程序接入示例/pages/index/index.js
@@ -0,0 +1,103 @@
+//index.js
+var okayapi = require('../../utils/okayapi.js')  
+
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: '正在调用小白接口……',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    console.log(app.globalData.okayapiHost)
+    this.okayApiHelloWorld()
+
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+
+  /**
+   * 小白接口请求示例
+   */
+  okayApiHelloWorld: function(e) {
+    /**
+     * 准备接口参数
+     */
+    let params = {
+      s: "Hello.World", 		// 必须，待请求的接口服务名称
+      name: "dogstar" 		  // 可选，根据接口文档，补充更多接口参数
+    };
+    let _self = this
+
+    /**
+     * 对小白接口发起请求
+     */
+    wx.request({
+      url: app.globalData.okayapiHost,
+      data: okayapi.enryptData(params),
+      success: function (wxRes) {
+        // TODO：实现你的逻辑……
+        let res = wxRes.data
+
+        if (res.data && res.data.err_code == 0) {
+          // TODO：请求成功
+          console.log('ok: ', res.data)
+
+          _self.setData({
+            motto: res.data.title
+          })
+        } else {
+          // TODO：当前操作失败
+          console.log('fail: ', res)
+
+          _self.setData({
+            motto: res.data.err_msg
+          })
+        }
+
+      }
+    })  
+  }
+
+})
